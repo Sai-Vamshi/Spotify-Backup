5efcbd0e8af569173419abacdcd60f1e15d066bb
awsec2app <yellowantapps@gmail.com>
2018-06-29 19:04:27 +0530

    9 commit

--- Spotify/__init__.py
+++ Spotify/__init__.py
@@ -7,6 +7,6 @@ if DEV_ENV=="HEROKU":
     base=os.environ.get("HEROKU_APP_NAME")
     website = "https://{}.herokuapp.com/".format(base)
     os.system("yellowant auth --token {} --host https://www.yellowant.com ".format(YA_DEVELOPER_TOKEN))
-    os.system('yellowant sync -q --api_url {}yellowant-api/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}yellowantredirecturl/'.format(website,website,website,website,website))
+    os.system('yellowant sync -q --api_url {}yellowant-api/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}yellowant-oauth-redirect/'.format(website,website,website,website,website))
     os.system('ls -al')
 
